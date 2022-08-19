e0419e7ebe9b6e08a45e0c779e4bb1cd35733701
zacjjohnson <wecooked@gmail.com>
2022-08-18 22:03:23 -0400

    text input working

--- scripts/boardClass.js
+++ scripts/boardClass.js
@@ -12,55 +12,17 @@ class Board extends Game{
     
             for(let j = 0; j < 5; j++){
                 let box = document.createElement('div');
-                box.className = `inputBoxes box`;
-
+                box.className = `inputBoxes`;
+                box.setAttribute('id', `box-${j}`);
+                box.setAttribute('maxLength', 1);
                 row.appendChild(box);
             }
             board.appendChild(row);
         }
     }
 
-    keyboard(){
-        document.addEventListener("keyup", (e) => {
-
-            if (guessesRemaining === 0) {
-                return
-            }
-        
-            let pressedKey = String(e.key)
-            if (pressedKey === "Backspace" && nextLetter !== 0) {
-                deleteLetter()
-                return
-            }
-        
-            if (pressedKey === "Enter") {
-                checkGuess()
-                return
-            }
-        
-            let found = pressedKey.match(/[a-z]/gi)
-            if (!found || found.length > 1) {
-                return
-            } else {
-                inputLetters(pressedKey)
-            }
-        })
-    }
-    inputLetters(){
-        if (nextLetter === 5) {
-            return
-        }
-        pressedKey = pressedKey.toLowerCase()
-    
-        let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining]
-        let box = row.children[nextLetter]
-        box.textContent = pressedKey
-        box.classList.add("filled-box")
-        currentGuess.push(pressedKey)
-        nextLetter += 1
-    };
 }
 
 let gameBoard = new Board();
 gameBoard.createBoard();
-gameBoard.inputLetters();
+
--- scripts/gameClass.js
+++ scripts/gameClass.js
@@ -2,6 +2,7 @@ class Game {
     constructor(wordArray){
         this.wordArray = wordArray;
         this.word = '';
+        // this.guess = guess;
         this.numberOfGuesses = 6;
         this.lettersInWord = 5;
         this.guessesRemaining = this.numberOfGuesses;
@@ -16,34 +17,61 @@ class Game {
         console.log(this.word);
     }
 
+    inputLetters(){
+        let currentBox = 0;
+        let guessedWord = [];
+        document.addEventListener("keyup", (event) => {
     
-
-    // compareLetters(word){
-    //     document.addEventListener("keyup", (event) => {
-    //         let firstBox = document.querySelector('.inputBoxes:first-child');
-    //         firstBox.innerHTML = event.key;
-    //         for(let i = 0; i < 5; i++){
-    //             let currentGuess = event.key;
-    //             let correctLetter = this.selectedWord[i];
-    //             if(currentGuess === correctLetter){
-                    
-    //                 console.log('True');
-    //             } else {
-    //                 console.log('false');
-    //             }
-    //         }
+            let letterInput = document.querySelector('#box-' + currentBox);
+            currentBox++
+            if(currentBox < 6 && event.code !== 'Enter'){
+                letterInput.innerHTML = event.key;
+                guessedWord.push(event.key);
+                // console.log(guessedWord);
+            } else {
+                return false;
+            }
+            console.log(guessedWord);
             
-                
-    //         });
-    // }
-
-    checkWord(){
+        });
         
     }
+
+    deleteLetter(){
+        document.addEventListener("keyup", (event) => {
+        if (event.code == "Backspace" && currentBox > 0) {
+            let letterInput = document.querySelector('#box-' + currentBox);
+            currentBox--
+            letterInput.innerHTML = '';
+        }
+    });
+    }
+
+    checkWord(){
+        document.addEventListener("keyup", (event) => {
+            if(event.code === 'Enter'){
+                console.log("Good Guess!");
+            } 
+        })
+    }
+
+    compareLetters(){
+        
+        if(this.guessedWord == 5){
+            console.log("It's fucking working");
+        }
+    }
+
+    
 }
 
 const game2 = new Game(wordsArray);
 
 
+
 game2.selectWord();
+game2.inputLetters();
+game2.deleteLetter();
+game2.checkWord();
+game2.compareLetters();
 // game2.compareLetters();
